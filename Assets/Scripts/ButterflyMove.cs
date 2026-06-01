e9623ef4582e67238efd06f50cf3c3414d6e609a
Muravska <muravska@sheridancollege.ca>
2026-05-31 23:11:22 -0400

    Butterfly is moving zig zag using lerp

--- Assets/Scripts/ButterflyMove.cs
+++ Assets/Scripts/ButterflyMove.cs
@@ -2,15 +2,54 @@ using UnityEngine;
 
 public class ButterflyMove : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public Transform pointA;
+    public Transform pointB;
+    public Transform pointC;
+    public Transform pointD;
 
-    // Update is called once per frame
+    public float speed = 0.5f;
+
+    float timer = 0f;
+    int currentMove = 1;
+
     void Update()
     {
-        
+        //Increase timer slowly
+        timer += Time.deltaTime * speed;
+
+        //Move zig zag
+        //Move from A to B
+        if (currentMove == 1)
+        {
+            transform.position = Vector3.Lerp(pointA.position, pointB.position, timer);
+
+            if (timer >= 1f)
+            {
+                timer = 0f;
+                currentMove = 2;
+            }
+        }
+        //Move from B to C
+        else if (currentMove == 2)
+        {
+            transform.position = Vector3.Lerp(pointB.position, pointC.position, timer);
+
+            if (timer >= 1f)
+            {
+                timer = 0f;
+                currentMove = 3;
+            }
+        }
+        //Move from C to D
+        else if (currentMove == 3)
+        {
+            transform.position = Vector3.Lerp(pointC.position, pointD.position, timer);
+
+            if (timer >= 1f)
+            {
+                timer = 0f;
+                currentMove = 4;
+            }
+        }
     }
 }
